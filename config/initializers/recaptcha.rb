193df2b97ad2d33535ee06afb13102f750e05dd5
Eric <Eric.Hainer@gmail.com>
2018-11-12 16:34:52 -0700

    [CHG] Set viewport width, fetch fallback recaptcha keys

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,8 +1,6 @@
 Recaptcha.configure do |config|
-  config.site_key = ENV.fetch('RECAPTCHA_SITE_KEY') do
-    Rails.application.credentials.recaptcha[:site_key] if Rails.env.development?
+  config.site_key = ENV.fetch('RECAPTCHA_SITE_KEY') { Rails.application.credentials.recaptcha[:site_key] }
   end
-  config.secret_key = ENV.fetch('RECAPTCHA_SECRET_KEY') do
-    Rails.application.credentials.recaptcha[:secret_key] if Rails.env.development?
+  config.secret_key = ENV.fetch('RECAPTCHA_SECRET_KEY') { Rails.application.credentials.recaptcha[:secret_key] }
   end
 end
