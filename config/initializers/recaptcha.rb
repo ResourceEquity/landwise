ec811391fd34c7b545cefa1c9c87e257a4e8d473
Eric <Eric.Hainer@gmail.com>
2018-11-12 15:25:07 -0700

    [CHG] Should not attempt to fetch credential information unless on development env

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,8 @@
 Recaptcha.configure do |config|
-  config.site_key = ENV.fetch('RECAPTCHA_SITE_KEY') { Rails.application.credentials.recaptcha[:site_key] }
-  config.secret_key = ENV.fetch('RECAPTCHA_SECRET_KEY') { Rails.application.credentials.recaptcha[:secret_key] }
+  config.site_key = ENV.fetch('RECAPTCHA_SITE_KEY') do
+    Rails.application.credentials.recaptcha[:site_key] if Rails.env.development?
+  end
+  config.secret_key = ENV.fetch('RECAPTCHA_SECRET_KEY') do
+    Rails.application.credentials.recaptcha[:secret_key] if Rails.env.development?
+  end
 end
