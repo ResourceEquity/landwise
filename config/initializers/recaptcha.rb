a93eea75538356accecde77a42d47515aaee333c
Eric <Eric.Hainer@gmail.com>
2018-11-12 16:36:36 -0700

    [TYPO] Remove stray ends

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,4 @@
 Recaptcha.configure do |config|
   config.site_key = ENV.fetch('RECAPTCHA_SITE_KEY') { Rails.application.credentials.recaptcha[:site_key] }
-  end
   config.secret_key = ENV.fetch('RECAPTCHA_SECRET_KEY') { Rails.application.credentials.recaptcha[:secret_key] }
-  end
 end
