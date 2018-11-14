7d0044d60ef90c83d5d2d963d8c0f33f2819bfb2
Eric <Eric.Hainer@gmail.com>
2018-11-14 10:55:05 -0700

    [CHG] Load sidekiq cron schedule file

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,9 +6,8 @@ end
 
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV['REDIS_PERSISTENT_URL'] }
+end
 
-
-  if File.exist?('config/schedule.yml') && Sidekiq.server?
-    Sidekiq::Cron::Job.load_from_hash YAML.load_file('config/schedule.yml')
-  end
+if File.exist?('config/schedule.yml') && Sidekiq.server?
+  Sidekiq::Cron::Job.load_from_hash YAML.load_file('config/schedule.yml')
 end
