6b319d19246d20503519e35cdd6fc02c8b25b32c
Eric <Eric.Hainer@gmail.com>
2018-11-14 11:09:28 -0700

    [CHG] Remove server? conditional check for sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -8,6 +8,6 @@ Sidekiq.configure_server do |config|
   config.redis = { url: ENV['REDIS_PERSISTENT_URL'] }
 end
 
-if File.exist?('config/schedule.yml') && Sidekiq.server?
+if File.exist?('config/schedule.yml')
   Sidekiq::Cron::Job.load_from_hash YAML.load_file('config/schedule.yml')
 end
